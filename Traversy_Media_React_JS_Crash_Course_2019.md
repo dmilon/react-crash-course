e5d6bdab3bd7035be13b588fb11f6a2f06fb65cb
damien <37899667+dmilon@users.noreply.github.com>
2019-01-25 09:05:44 +0100

    git init

---
+++ Traversy_Media_React_JS_Crash_Course_2019.md
@@ -0,0 +1,6 @@
+# To learn (ES 6)
+* Classes
+* Destructuring
+* High order array methods - forEach, map, filter
+* Arrow functions
+* Fetch API & promises
--- src/App.js
+++ src/App.js
@@ -1,25 +1,62 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import Header from './components/layout/Header';
+import Todos from './components/Todos';
+import AddTodo from './components/AddTodo';
 import './App.css';
 
 class App extends Component {
+  state = {
+    todos: [
+    {
+      id: 1,
+      title: 'Take out the trash',
+      completed: false
+    },
+        {
+      id: 2,
+      title: 'Dinner with wife',
+      completed: false
+    },
+        {
+      id: 3,
+      title: 'Meeting with boss',
+      completed: false
+    }
+    ]
+  }
+
+  // Toggle complete
+  markComplete = (id) => {
+    this.setState({todos: this.state.todos.map(todo => {
+      if(todo.id === id) {
+        todo.completed = !todo.completed
+      }
+      return todo;
+    })})
+  }
+
+  delTodo = (id) => {
+    this.setState({todos: [...this.state.todos.filter(todo => todo.id !== id)]})
+  }
+
+  addTodo = (title) => {
+    const newTodo = {
+      id: 4,
+      title,
+      completed: false
+    }
+    this.setState({ todos: [...this.state.todos, newTodo] })
+  }
+
   render() {
+    // console.log(this.state.todos)
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <div className="container">
+          <Header />
+          <AddTodo addTodo={this.addTodo}/>
+          <Todos todos={this.state.todos} markComplete={this.markComplete} delTodo={this.delTodo}/>
+        </div>
       </div>
     );
   }
